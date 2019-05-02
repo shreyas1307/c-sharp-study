594216f813d728470d39e8392a8311bd56cf8784
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-05-02 22:13:51 +0530

    More C# Learning

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string myName = "Shreyas Balachandran";
+            int age = 23;
+            string aboutMe = "Self-Taught Software Developer";
+            Console.WriteLine("Hello, Welcome to my C# Program. Please press any key to continue");
+            Console.ReadLine();
+            Console.WriteLine("Hello World from C#." + " My name is " + myName + "I am " + age + " years old. I am a " + aboutMe + ".");
+            Console.ReadLine();
+
+
+        }
+    }
+}
